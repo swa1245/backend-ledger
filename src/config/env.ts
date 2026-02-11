884d9f9c842d596869971bfbb789b0a198087616
swa1245 <swarajpatil2123@gmail.com>
2026-02-11 23:27:12 +0530

    added eamil setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,5 +4,9 @@ export const env ={
     PORT : process.env.PORT,
     MONGO_URI : process.env.MONGO_URI,
     NODE_ENV : process.env.NODE_ENV,
-    JWT_SECRET : process.env.JWT_SECRET
+    JWT_SECRET : process.env.JWT_SECRET,
+    CLIENT_ID : process.env.CLIENT_ID,
+    CLIENT_SECRET : process.env.CLIENT_SECRET,
+    REFRESH_TOKEN : process.env.REFRESH_TOKEN,
+    EMAIL_USER : process.env.EMAIL_USER,
 }
